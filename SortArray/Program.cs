9a408ffdef94d40ec4ef7f2e596fe62c49bd1398
iyosiyas wolde <iyosiyasbeyene@yahoo.com>
2018-02-13 14:55:12 -0800

    sortarrayfirst

--- SortArray/Program.cs
+++ SortArray/Program.cs
@@ -6,7 +6,15 @@ namespace SortArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			int[]n = { -4, 5, 2, -6, 45, 34, -12, 56, 20 };			
+				Array.Sort(n);
+			 int i= 0;
+			for(i = 0; i < n.Length; i++)  
+			
+				Console.WriteLine(n[i]);
+				Console.ReadLine();
+			
+			
         }
     }
 }
